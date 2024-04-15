706e21293c344520a5a2f362d2653607426e9a15
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-15 22:23:29 +0000

    edited answer.php

--- answer.php
+++ answer.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Volume of a Triangle, in PHP" />
+  <meta name="description" content="Volume of a Sphere, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,22 +13,22 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Volume of a Pyramid, in PHP</title>
+  <title>Volume of a Sphere, in PHP</title>
 </head>
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Volume of a Pyramid, in PHP</span>
+        <span class="mdl-layout-title">Volume of a Sphere, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/pyramid (1).png" alt="pyramid" width="250" />
+        <img src="./images/sphere.png" alt="sphere" width="250" />
       </div>
       <div class="page-content-php">
-        <div name="volume-of-pyramid">
+        <div name="volume-of-sphere">
           <div class="mdl-layout__header-row">
             <br />
             <div class="mdl-layout__content">
@@ -38,15 +37,13 @@
               <?php
 
               // input
-              $length = $_POST["length"];
-              $width = $_POST["width"];
-              $height = $_POST["height"];
+              $radius = $_POST["radius"];
 
               // process
-              $volume = ($length * $width * $height) / 3;
+              $volume = (4 / 3) * Math.PI * Math.pow($radius, 3)
 
               // output
-              echo "If the length is " . $length . " and the width is " . $width . " and the height is " . $height . ". ";
+              echo "If the radius is " . $radius . ". ";
               echo "<br />";
               echo "<br />";
               echo "Then the Volume is: " . round($volume, 2) . " mm³";
